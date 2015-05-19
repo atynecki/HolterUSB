c0be6487b86319bc5d664979ff2d04a63e9d5319
ArturT <a.tynecki@intelclinic.com>
2015-05-19 21:43:23 +0200

    first commit

---
+++ Holter_USB/Debug/hal.pp
@@ -0,0 +1,131 @@
+# FIXED
+
+hal.obj: ../hal.c
+hal.obj: E:/Programs/TI/ccsv6/ccs_base/msp430/include/msp430.h
+hal.obj: E:/Programs/TI/ccsv6/ccs_base/msp430/include/msp430f5529.h
+hal.obj: E:/Programs/TI/ccsv6/ccs_base/msp430/include/in430.h
+hal.obj: E:/Programs/TI/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/intrinsics.h
+hal.obj: E:/Programs/TI/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/intrinsics_legacy_undefs.h
+hal.obj: E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/driverlib.h
+hal.obj: E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/inc/hw_memmap.h
+hal.obj: E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/inc/../deprecated/CCS/msp430f5xx_6xxgeneric.h
+hal.obj: E:/Programs/TI/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/stdint.h
+hal.obj: E:/Programs/TI/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/stdbool.h
+hal.obj: E:/Programs/TI/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/yvals.h
+hal.obj: E:/Programs/TI/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/stdarg.h
+hal.obj: E:/Programs/TI/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/linkage.h
+hal.obj: E:/Programs/TI/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/_lock.h
+hal.obj: E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/adc10_a.h
+hal.obj: E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/adc12_a.h
+hal.obj: E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/inc/hw_regaccess.h
+hal.obj: E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/aes.h
+hal.obj: E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/battbak.h
+hal.obj: E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/comp_b.h
+hal.obj: E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/crc.h
+hal.obj: E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/dac12_a.h
+hal.obj: E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/dma.h
+hal.obj: E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/eusci_a_spi.h
+hal.obj: E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/eusci_a_uart.h
+hal.obj: E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/eusci_b_i2c.h
+hal.obj: E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/eusci_b_spi.h
+hal.obj: E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/eusci_i2c.h
+hal.obj: E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/eusci_spi.h
+hal.obj: E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/eusci_uart.h
+hal.obj: E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/flashctl.h
+hal.obj: E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/gpio.h
+hal.obj: E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/ldopwr.h
+hal.obj: E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/mpy32.h
+hal.obj: E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/pmap.h
+hal.obj: E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/pmm.h
+hal.obj: E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/ram.h
+hal.obj: E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/ref.h
+hal.obj: E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/rtc_a.h
+hal.obj: E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/rtc_b.h
+hal.obj: E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/rtc_c.h
+hal.obj: E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/sd24_b.h
+hal.obj: E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/sfr.h
+hal.obj: E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/sysctl.h
+hal.obj: E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/tec.h
+hal.obj: E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/timer_a.h
+hal.obj: E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/timer_b.h
+hal.obj: E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/timer_d.h
+hal.obj: E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/tlv.h
+hal.obj: E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/ucs.h
+hal.obj: E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/usci_a_spi.h
+hal.obj: E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/usci_a_uart.h
+hal.obj: E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/usci_b_i2c.h
+hal.obj: E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/usci_b_spi.h
+hal.obj: E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/usci_i2c.h
+hal.obj: E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/usci_spi.h
+hal.obj: E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/usci_uart.h
+hal.obj: E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/wdt_a.h
+hal.obj: ../USB_API/USB_Common/device.h
+hal.obj: ../USB_config/descriptors.h
+hal.obj: E:/Projects/C/C0_SimpleSend/USB_API/USB_Common/usb.h
+hal.obj: ../hal.h
+hal.obj: E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/driverlib.h
+
+../hal.c: 
+E:/Programs/TI/ccsv6/ccs_base/msp430/include/msp430.h: 
+E:/Programs/TI/ccsv6/ccs_base/msp430/include/msp430f5529.h: 
+E:/Programs/TI/ccsv6/ccs_base/msp430/include/in430.h: 
+E:/Programs/TI/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/intrinsics.h: 
+E:/Programs/TI/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/intrinsics_legacy_undefs.h: 
+E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/driverlib.h: 
+E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/inc/hw_memmap.h: 
+E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/inc/../deprecated/CCS/msp430f5xx_6xxgeneric.h: 
+E:/Programs/TI/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/stdint.h: 
+E:/Programs/TI/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/stdbool.h: 
+E:/Programs/TI/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/yvals.h: 
+E:/Programs/TI/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/stdarg.h: 
+E:/Programs/TI/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/linkage.h: 
+E:/Programs/TI/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/_lock.h: 
+E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/adc10_a.h: 
+E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/adc12_a.h: 
+E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/inc/hw_regaccess.h: 
+E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/aes.h: 
+E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/battbak.h: 
+E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/comp_b.h: 
+E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/crc.h: 
+E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/dac12_a.h: 
+E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/dma.h: 
+E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/eusci_a_spi.h: 
+E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/eusci_a_uart.h: 
+E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/eusci_b_i2c.h: 
+E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/eusci_b_spi.h: 
+E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/eusci_i2c.h: 
+E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/eusci_spi.h: 
+E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/eusci_uart.h: 
+E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/flashctl.h: 
+E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/gpio.h: 
+E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/ldopwr.h: 
+E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/mpy32.h: 
+E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/pmap.h: 
+E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/pmm.h: 
+E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/ram.h: 
+E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/ref.h: 
+E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/rtc_a.h: 
+E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/rtc_b.h: 
+E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/rtc_c.h: 
+E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/sd24_b.h: 
+E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/sfr.h: 
+E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/sysctl.h: 
+E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/tec.h: 
+E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/timer_a.h: 
+E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/timer_b.h: 
+E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/timer_d.h: 
+E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/tlv.h: 
+E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/ucs.h: 
+E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/usci_a_spi.h: 
+E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/usci_a_uart.h: 
+E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/usci_b_i2c.h: 
+E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/usci_b_spi.h: 
+E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/usci_i2c.h: 
+E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/usci_spi.h: 
+E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/usci_uart.h: 
+E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/wdt_a.h: 
+../USB_API/USB_Common/device.h: 
+../USB_config/descriptors.h: 
+E:/Projects/C/C0_SimpleSend/USB_API/USB_Common/usb.h: 
+../hal.h: 
+E:/Projects/C/C0_SimpleSend/driverlib/MSP430F5xx_6xx/driverlib.h: 
